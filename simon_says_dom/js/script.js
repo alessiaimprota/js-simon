8aee5942e8e3103dd84d46758210c8a68bf1ce95
alessiaimprota <alessiaimprota126@gmail.com>
2025-12-12 18:01:13 +0100

    almost done

--- simon_says_dom/js/script.js
+++ simon_says_dom/js/script.js
@@ -14,21 +14,56 @@ for (let index = 0; index < arrayRandomGame.length; index++) {
 
 //Definisco la mia varabiale per un output del countdown
 const outputCounter = document.getElementById("countdown");
-let seconds = 10;
+let seconds = 5;
 
-const timer= setInterval( function() {
-    outputCounter.innerText=seconds;
-    if(seconds===0){
-        clearInterval(timer)
-        outputCounter.classList.add("d-none")
-        document.getElementById("answers-form").classList.remove("d-none");
-        randomNumbers.classList.add("d-none");
+const timer = setInterval(function () {
+  outputCounter.innerText = seconds;
+  if (seconds === 0) {
+    clearInterval(timer);
+    outputCounter.classList.add("d-none");
+    document.getElementById("answers-form").classList.remove("d-none");
+    randomNumbers.classList.add("d-none");
+  } else {
+    seconds--;
+  }
+  console.log(timer);
+}, 1000);
+
+//confrontare i numeri dati dall'utente con il valore con i numeri generati random
+const form = document.querySelector("form");
+const inputUserArray = [];
+const result= []
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputs = document.querySelectorAll(".form-control");
+
+  for (let index = 0; index < inputs.length; index++) {
+    console.log(inputs[index]);
+    inputUserArray.push(inputs[index].value);
+  }
+
+  //Creo un altro array che contiene le parole in comune tra i due array
+    for (let index = 0; index < inputUserArray.length; index++) {
+        if((arrayRandomGame).includes(parseInt(inputUserArray[index]))){
+            result.push(inputUserArray[index])
+        }   
+    }
+    
+    let message = document.getElementById("message")
+
+    if(result.length===arrayRandomGame.length){
+        message.innerText=`Bravo! Hai indovinato ${result.length} numeri! ${result}`
+        message.classList.remove("text-danger")
+        message.classList.add("text-success")
+    }
+    else if(result.length<arrayRandomGame.length && result.length!==0){
+        message.innerText=`Hai indovinato ${result.length} numeri! ${result}`
+        message.classList.remove("text-danger")
+        message.classList.add("text-secondary")
     }
     else{
-        seconds--;
-    }
-    console.log(timer)
-}, 1000);
+         message.innerText=`Hai indovinato ${result.length} numeri!`}
+});
 
 
 //-Definire una funzione che mi generi numeri random
@@ -48,6 +83,5 @@ function arrayWithRandomNumbers(minNum, maxNum, totNum) {
       randomNumArray.push(randomNum);
     }
   }
-
   return randomNumArray;
 }
